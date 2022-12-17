5873a1dc3ba1f6e7698aa81b35a6f3257d7800de
aaqib <aaqibnadeem123@gmail.com>
2022-12-18 01:34:18 +0500

    initialize repo

---
+++ src/NavBar/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+  // {
+  //   title: "Swap",
+  //   url: "/swap",
+  //   cName: "nav-links",
+  // },
+  // {
+  //   title: "Liquidity",
+  //   url: "/Alternative-Uniswap-Interface/liquidity",
+  //   cName: "nav-links",
+  // },
+];
