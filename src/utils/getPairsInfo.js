0b0ab2da23df1b6471540454892af0c53baf39b3
aaqib <aaqibnadeem123@gmail.com>
2022-12-29 15:44:08 +0500

    order pair change

--- src/utils/getPairsInfo.js
+++ src/utils/getPairsInfo.js
@@ -1,7 +1,12 @@
 import { abis } from "./../contracts";
 
 export const getPairsInfo = async (pairAddressesOld, web3) => {
-    let pairAddresses = ['0x4c32dCc8138bbD129E54cED488298bD73f71cC91', '0x3d2180DB9E1B909f35C398BC39EF36108C0FC8c3', '0x8898f7742FC132a009Bc3EEecE8a1d460ADFb584', '0xDCB68AbA4c9B87C76fA6ADB54214451e91bFD1bC',
+    let pairAddresses = [
+        '0x4c32dCc8138bbD129E54cED488298bD73f71cC91', 
+        '0x3d2180DB9E1B909f35C398BC39EF36108C0FC8c3', 
+        '0x8898f7742FC132a009Bc3EEecE8a1d460ADFb584', 
+        '0xDCBEe84e534c8aC458Ef0F57E2137f086e034F09',
+        '0xDCB68AbA4c9B87C76fA6ADB54214451e91bFD1bC',
         '0xbA452A1c0875D33a440259B1ea4DcA8f5d86D9Ae',
         '0xC58cE0D8E2739bF0B587Fe7d664E18b097358121',
         '0x00FcaF5fE48D2a91D802701f72d3934F8B4352cc',
@@ -17,8 +22,7 @@ export const getPairsInfo = async (pairAddressesOld, web3) => {
         '0x4B377121d968Bf7a62D51B96523d59506e7c2BF0',
         '0x8bC556Cb72e7D77f79f9a3f5C4E27a22E60F20cf',
         '0x18AE87AE14279D42fCdde542c7Faa65768434024',
-        '0x15fF6fB9D18AD509ae67aC81ce155a4341295299',
-        '0xDCBEe84e534c8aC458Ef0F57E2137f086e034F09'
+        '0x15fF6fB9D18AD509ae67aC81ce155a4341295299'
     ];
     const pairsInfo = [];
     const pairABI = abis.pair;
